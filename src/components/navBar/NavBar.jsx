81fa62bfcb23d6cf3322f08004ff60a0cdd4ca21
mahmoudadel54 <mahmoodmadel@gmail.com>
2022-01-18 23:05:27 +0200

    edit navbar content

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -6,28 +6,7 @@ function NavBar() {
       <Link className="btn btn-secondary" to="/">
         <i className="fas fa-home"></i> Home
       </Link>
-      <span>Simple App</span>
-      {/* <Link
-        className="btn btn-success"
-        style={{ margin: "0 0.2vw" }}
-        to="/users"
-      >
-        Users
-      </Link>
-      <Link
-        className="btn btn-secondary"
-        style={{ margin: "0 0.2vw" }}
-        to="/comments"
-      >
-        Comments
-      </Link>
-      <Link
-        className="btn btn-danger"
-        style={{ margin: "0 0.2vw" }}
-        to="/images"
-      >
-        Images
-      </Link> */}
+      <span>Simple App With OpenLayers + ReactJS</span>
     </div>
   );
 }
